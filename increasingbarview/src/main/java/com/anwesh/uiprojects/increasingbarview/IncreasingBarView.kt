0dcd3a03d67eea2b4461beeb561d072f336205c1
Anwesh43 <anweshthecool0@gmail.com>
2018-08-28 11:07:04 +0530

    adding LinkedIncreasingBar class

--- increasingbarview/src/main/java/com/anwesh/uiprojects/increasingbarview/IncreasingBarView.kt
+++ increasingbarview/src/main/java/com/anwesh/uiprojects/increasingbarview/IncreasingBarView.kt
@@ -136,4 +136,27 @@ class IncreasingBarView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LinkedIncreasingBar(var i : Int) {
+
+        private var curr : IBNode = IBNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
