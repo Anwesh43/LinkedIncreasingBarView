b61a0a13aa1c63a8d4950abe5d8ddc8e6504303c
Anwesh43 <anweshthecool0@gmail.com>
2018-08-28 10:51:21 +0530

    adding libraries and defining nodes

--- increasingbarview/src/main/java/com/anwesh/uiprojects/increasingbarview/IncreasingBarView.kt
+++ increasingbarview/src/main/java/com/anwesh/uiprojects/increasingbarview/IncreasingBarView.kt
@@ -3,3 +3,11 @@ package com.anwesh.uiprojects.increasingbarview
 /**
  * Created by anweshmishra on 28/08/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+
+val nodes : Int = 5 
