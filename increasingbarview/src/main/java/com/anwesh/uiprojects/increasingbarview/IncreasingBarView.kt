03d12e6d9a1ff2edf4b7a418b54833cd1e8f09fa
Anwesh43 <anweshthecool0@gmail.com>
2018-08-28 10:57:52 +0530

    adding IncreasingBarView class

--- increasingbarview/src/main/java/com/anwesh/uiprojects/increasingbarview/IncreasingBarView.kt
+++ increasingbarview/src/main/java/com/anwesh/uiprojects/increasingbarview/IncreasingBarView.kt
@@ -29,3 +29,21 @@ fun Canvas.drawIBNode(i : Int, scale : Float, paint : Paint) {
     drawRect(RectF(0f,-hRect/2, gap * sc, hRect/2), paint)
     restore()
 }
+
+class IncreasingBarView(ctx : Context) : View(ctx) {
+
+    private val  paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
