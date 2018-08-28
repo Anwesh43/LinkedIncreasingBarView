b580da049cddeb18cd7392375681c24276c9bb1e
Anwesh43 <anweshthecool0@gmail.com>
2018-08-28 11:21:18 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedincreasingbarview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedincreasingbarview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedincreasingbarview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.increasingbarview.IncreasingBarView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        IncreasingBarView.create(this)
+        fullScreen()
+    }
+
+    fun MainActivity.fullScreen() {
+        supportActionBar?.hide()
+        window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
